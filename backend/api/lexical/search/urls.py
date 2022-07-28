c5a2b0d1562e5b1d92a33d5966e81ff081bcc496
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-07-28 17:32:37 +0200

    Add more urls and modify the current

--- backend/api/lexical/search/urls.py
+++ backend/api/lexical/search/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import *
 
-app_name = 'QuranAPI'
+app_name = 'QuranLexicalSearchAPI'
 
 urlpatterns = [
     # API views
     path('', api_docs, name='api_docs'),
-    path('api/lexical/all-verses', all_verses_api, name='all_verses_api'),
-    path('api/lexical/search-word/<str:words>', search_word_api, name='search_word_api'),
-    path('api/lexical/get-surah/<int:surah_id>', get_surah_api, name='get_surah_api'),
-    path('api/lexical/get-verse/<int:surah_id>/<int:verse_id>', get_verse_api, name='get_verse_api'),
+    path('api/lexical/all', get_all, name='get_all'),
+    path('api/lexical/search/<str:words>', search, name='search'),
+    path('api/lexical/surah/<int:surah_id>', get_surah, name='get_surah'),
+    path('api/lexical/verse-in-surah/<int:surah_id>/<int:verse_id>', get_verse_in_surah, name='get_verse_in_surah'),
+    path('api/lexical/verse-in-quran/<int:verse_id>', get_verse_in_quran, name='get_verse_in_quran'),
 ]
--- backend/api/lexical/search/views.py
+++ backend/api/lexical/search/views.py
@@ -7,46 +7,44 @@ from django.shortcuts import get_object_or_404, render
 
 
 @api_view(['GET'])
-def all_verses_api(request) -> None:
+def get_all(request) -> None:
     """
         Returns all verses in Database.
         Not really effective for search, so use it to clone the database or such on.
     """
     verses = Verses.objects.all()
     data = VersesSerializers(verses, many=True).data
-    print(verses)
-    print(data)
     return Response({'length': len(data), 'data': data})
 
 
 @api_view(['GET'])
-def search_word_api(request, words) -> str:
+def search(request, words) -> str:
     """
-        Search in verses using icontains.
+        Search in verses using insensitive contains.
         Return all verses with given words.
     """
     verses = Verses.objects.filter(
         Q(verse__icontains=words) | Q(verseWithoutTashkeel__icontains=words)
     )
     data = VersesSerializers(verses, many=True).data
-    return Response({'resultsLength': len(data), 'data': data})
+    return Response({'length': len(data), 'data': data})
 
 
 @api_view(['GET'])
-def get_surah_api(request, surah_id) -> int:
+def get_surah(request, surah_id) -> int:
     """
         Retrieving surah_pk, searching in database with verse_pk:
         verse_pk: S***V*** | surah_pk -> first part of verse_pk (S***).
-        Returns all Verses of Surah.
+        Returns all Verses of a Surah.
     """
     surah_pk = f"S{str(surah_id).zfill(3)}"
     verse_id = Verses.objects.filter(verse_pk__icontains=surah_pk)
     data = VersesSerializers(verse_id, many=True).data
-    return Response({'versesNumber': len(data), 'data': data})
+    return Response({'length': len(data), 'data': data})
 
 
 @api_view(['GET'])
-def get_verse_api(request, surah_id, verse_id) -> int:
+def get_verse_in_surah(request, surah_id, verse_id) -> int:
     """
         Taking two integers and convert them to verse_pk,
         to search in database with it using get_object_or_404.
@@ -54,6 +52,18 @@ def get_verse_api(request, surah_id, verse_id) -> int:
     """
     verse_pk = f'S{str(surah_id).zfill(3)}V{str(verse_id).zfill(3)}'
     verse = get_object_or_404(Verses, verse_pk=verse_pk)
+    data = VersesSerializers(verse).data
+    return Response({'data': data})
+
+
+@api_view(['GET'])
+def get_verse_in_quran(request, verse_id) -> int:
+    """
+        Taking verse_id of all Quran, 
+        to search in database with it using get_object_or_404.
+        Returns the Verse.
+    """
+    verse = get_object_or_404(Verses, numberInQuran=verse_id)
     data = VersesSerializers(verse).data
     return Response({'data': data})
 
