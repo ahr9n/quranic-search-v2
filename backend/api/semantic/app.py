02fb01bb48b3cda0e1c3d5f1b9ebd5834cdc3891
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-07-29 02:44:06 +0200

    Add and allow flask_cors for all urls

--- backend/api/semantic/app.py
+++ backend/api/semantic/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from flask_restful import Api
 # semantic/src/models/predict.py
 from src.models.predict import MostSimilarWord , MostSimilarVerse
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
+
+# This will enable CORS for all routes
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 api = Api(app)
 
