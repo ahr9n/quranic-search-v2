07c585f42e8d09d169f7009fe81d803561f09a8a
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-30 01:07:33 +0200

    Add exit script

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,8 +5,10 @@ python3 -m pip install --upgrade pip
 
 # Download the pre-trained models (AraVec cbow_100_twitter for now)
 cd backend/api/semantic/models || exit
-wget https://bakrianoo.ewr1.vultrobjects.com/aravec/full_grams_cbow_100_twitter.zip
-unzip full_grams_cbow_100_twitter.zip
+if ! [[ -f ./full_grams_cbow_100_twitter.zip ]]; then
+	wget https://bakrianoo.ewr1.vultrobjects.com/aravec/full_grams_cbow_100_twitter.zip
+	unzip full_grams_cbow_100_twitter.zip
+fi
 cd ../../../../
 
 # Also, KSUCCA full_cbow model is light to use, however, you will need to configure it manually in the code
