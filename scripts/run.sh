1ff4dc693ae2b81eff6a9e7d693719b800bcc5eb
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-30 00:10:16 +0200

    Clean up

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,9 @@
 
 # Run the lexical/semantic API locally
 python3 backend/api/lexical/manage.py runserver &
+sleep 5s
 python3 backend/api/semantic/app.py &
 
 # Run the website frontend locally
 cd frontend || exit
 npm start
-
-# Visit the website
-xdg-open http://localhost:3000 &
